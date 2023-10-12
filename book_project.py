5f7dc75fa31eaedf6caeea6dede111e88011fc05
Christfel-Sugarol <97380447+Christfel-Sugarol@users.noreply.github.com>
2023-10-12 08:43:07 +0800

    Updated book_project.py

--- book_project.py
+++ book_project.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 class book:
     def __init__(self,title,author,year,status):
@@ -6,12 +7,11 @@ class book:
         self.author = author
         self.year = year
         self.status = status
+        self.ID = datetime.now()
 
     def getAllValues(self):
-        return [self.title, self.author, self.year,self.status]    
-    
-
-
+        return [self.title, self.author, self.year,self.status, self.ID]   
+ 
 books = []
 
 def searchByPara(para, mode, num = 0, book_list = []):
@@ -27,9 +27,7 @@ def searchByPara(para, mode, num = 0, book_list = []):
             else:
                 book_status = "Checked out"    
 
-
-            print("Title: {} || Author: {} || Year of Publication: {} || Status: {}".format(i.getAllValues()[0], i.getAllValues()[1], i.getAllValues()[2], book_status))
-        
+            print("Title: {} || Author: {} || Year of Publication: {} || Status: {} || ID: <{}>".format(i.getAllValues()[0], i.getAllValues()[1], i.getAllValues()[2], book_status, i.getAllValues()[4]))
 
         for i in reversed(book_list):
             book_list.remove(i)
@@ -52,8 +50,8 @@ def searchByTitle(title):
 
 def add_book():
     print("Currrently adding a book...")
-    title = raw_input("Enter the title: ")
-    author = raw_input("Enter the author: ")
+    title = str(raw_input("Enter the title: "))
+    author = str(raw_input("Enter the author: "))
     
     year = ""
 
@@ -61,21 +59,27 @@ def add_book():
         year = raw_input("Enter the year of publiction: ")
         if year.isdigit():
             break
+        else:
+            print("Please enter a valid number!!!")
+
+    year = int(year)        
+
 
     status = True
-
-    books.append(book(title, author, year, status))
+    new_book = book(title, author, year, status)
+    books.append(new_book)
+    print("Successfully added. Book ID: <{}>".format(new_book.getAllValues()[4]))
 
 def checkout_book():
     print("Currently checking out a book...")
     user_input = raw_input("Enter the title: ")
 
     if(not searchByTitle(user_input)):
-        print("not in library")
+        print("Book not in library")
 
     elif (searchByTitle(user_input).status == True):
         searchByTitle(user_input).status = False
-        print("successfully checked out")
+        print("Successfully checked out")
               
     else:
         print("Book has been already checked out")
@@ -85,7 +89,7 @@ def return_book():
     user_input = raw_input("Enter the title: ")
 
     if(not searchByTitle(user_input)):
-        print("not in library")
+        print("Book not in library")
 
     elif (searchByTitle(user_input).status == False):
         searchByTitle(user_input).status = True
@@ -94,17 +98,56 @@ def return_book():
     else:
         print("Book is already in library")
 
+
+def searchByID(ID):
+    for i in books:
+        if str(i.ID) == str(ID):
+            return i
+    return False        
+
+def checkout_by_id():
+    print("Currently checking out a book...")
+    user_input = str(raw_input("Enter the Book ID: "))
+    input = searchByID(user_input)
+
+    if(not input):
+        print("Book not in library")
+
+    elif (input.status == True):
+        input.status = False
+        print("Successfully checked out")
+              
+    else:
+        print("Book has been already checked out")     
+
+def return_by_id():
+    print("Currently returning a book...")
+    user_input = str(raw_input("Enter the Book ID: "))
+    input = searchByID(user_input)
+
+    if(not input):
+        print("Book not in library")
+
+    elif (input.status == False):
+        input.status = True
+        print("Successfully returned")
+              
+    else:
+        print("Book is already in library")        
+
 def intro():
     time.sleep(0.5)
     print("")
     print("-------------------------------------")
     print("1. Add a book")
-    print("2. Check out a book")
-    print("3. Return a book")
-    print("4. Search books by author")
-    print("5. Search books by year of publication")
-    print("6. Check books by status")
-    print("7. Exit ")
+    print("2. Check out a book by Title")
+    print("3. Check out a book by ID")
+    print("4. Return a book by Title")
+    print("5. Return a book by ID")
+    print("6. Search books by author")
+    print("7. Search books by year of publication")
+    print("8. Check books by status")
+    print("9. Exit ")
 
 while True:
     choice = ""
@@ -117,7 +160,7 @@ while True:
         if choice.isdigit():
             choice = int(choice)
 
-            if (choice >= 1) and (choice <= 7):
+            if (choice >= 1) and (choice <= 9):
                 break
             else:
                 print("Enter a number within 1 to 7!!!")
@@ -132,10 +175,15 @@ while True:
     elif choice == 2:
         checkout_book()
     elif choice == 3:
-        return_book()
+        checkout_by_id()
     elif choice == 4:
-        searchByPara(raw_input("Enter the name of the author: "), 1)
+        return_book()
     elif choice == 5:
+        return_by_id()
+
+    elif choice == 6:
+        searchByPara(raw_input("Enter the name of the author: "), 1)
+    elif choice == 7:
         while True:
             year = raw_input("Enter the year of publiction: ")
             if year.isdigit():
@@ -144,8 +192,7 @@ while True:
                 print("Enter a valid number!!!")
 
         searchByPara(year, 2)
-
-    elif choice == 6:
+    elif choice == 8:
         while True:
             book_status = raw_input("Enter the status of the book - available? (Y/N): ")
 
@@ -161,13 +208,4 @@ while True:
         
         searchByPara(book_status, 3)
     else:
-        break       
-
-
-
-
-
-
-
-
-#print(searchByTitle("Alice in Wonderland"))     
+        break
